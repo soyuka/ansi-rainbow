2505f3dbfbb927e6747a99b54567e42c91ea63a8
soyuka <soyuka@gmail.com>
2014-04-30 13:02:10 +0200

    This is now the most advanced nodejs rainbow tool :beer: - r(string)           rainbowify - bg(string)          background-rainbowify - add(colors)         do not skip colors - skip(colors)        skip colors - reset()             reset colors (skips ['black', 'white', 'bgBlack', 'bgWhite']) - options({})         set options - options are (bool) color_space, (int) gap

--- index.js
+++ index.js
@@ -1,74 +1,191 @@
-var ansi = require('ansi-styles');
+var ansi = require('ansi-styles')
 
 /**
- * black, white and gray are not included
+ * black, white and gray (buggy?) are not included
  */
 var Pony = function() {
 	this._colors = [
-				'red',
-				'green',
-				'yellow',
-				'blue',
-				'magenta',
-				'cyan',
-			]
-	this._colors_num = this._colors.length - 1
-	this._next = 0
-	this._prev = 0
+    'red',
+		'green',
+    'yellow',
+		'blue',
+		'magenta',
+		'cyan',
+    'white',
+    'black'
+	]
 
-	return this
+  this._colors_num = this._colors.length
+
+  this._backgrounds = []
+
+  for(var i = 0; i < this._colors_num; i++) {
+    this._backgrounds[i] = 'bg'+this._colors[i].charAt(0).toUpperCase()+this._colors[i].slice(1)
+  }
+
+  this._skip = ['black', 'white', 'bgBlack', 'bgWhite']
+  this._skip_num = this._skip.length - 1
+
+	this._next = 0
+	this._prev = -1
+
+  this.options = {color_space: false, gap: 1}
+
+  var self = this
+
+  this.wrapper = {
+    bg: this.ponyfy(true),
+    r: this.ponyfy(),
+    add: this.addorskip('add'),
+    skip: this.addorskip('skip'),
+    options: function(opts) {
+      for(var i in opts)
+        self.options[i] = opts[i]
+      return self.wrapper
+    },
+    colors: function() {
+      return self.colors.call(self)
+    },
+    reset: function() {
+      self._skip = ['black', 'white', 'bgBlack', 'bgWhite']
+      return self.wrapper
+    },
+    _colors: this._colors,
+    _backgrounds: this._backgrounds
+  }
+
+  return this.wrapper
+
 }
 
 Pony.prototype = {
-	bgify: function(color) {
-		return 'bg'+color.charAt(0).toUpperCase()+color.slice(1)
-	},
+  top: function(color) {
+
+    var index = this._colors.indexOf(color)
+
+    if(~index) {
+      this._colors.splice(index, 1)
+      this._colors.unshift(color)  
+    }
+
+    index = this._backgrounds.indexOf(color)
+    if(~index) {
+      this._backgrounds.splice(index, 1)
+      this._backgrounds.unshift(color)  
+    }
+      
+    return this
+  },
+  colors: function(bg) {
+    var res = [], 
+        items = bg ? this._backgrounds : this._colors,
+        l = items.length
+
+    for(var i = 0; i < l; i++) {
+      if(this._skip.indexOf(items[i]) === -1)
+        res.push(items[i])
+    }
+    
+    return res;
+  },
+  allowed: function(color) {
+    return this._colors.indexOf(color) !== -1 || this._backgrounds.indexOf(color) !== -1
+  },
+  addorskip: function(which, wrapper) {
+    var self = this
+    return function() {
+
+      colors = [].slice.call(arguments)
+      colors = colors.length == 1 ? colors[0] : colors
+      colors = 'string' == typeof colors ? colors.split(' ') : colors
+      colors = colors instanceof Array ? colors : [colors], l = colors.length
+
+       while(l--) {
+
+        if(self.allowed(colors[l])) {
+          index = self._skip.indexOf(colors[l])
+
+          if(index === -1 && which === 'skip') {
+            self._skip_num++
+            self._skip[self._skip_num] = colors[l]
+          }
+          else if(~index && which === 'add') {
+            self.top(colors[l])
+            self._skip.splice(index, 1)
+            self._skip_num--
+          }
+        } else {
+          //console.log(colors[l])
+          throw new Error('Color '+ colors[l] +' is not recognized')
+        }
+      }
+      
+      return self.wrapper
+    }
+  },
   //wrapper to pass bg
   ponyfy: function(bg) {
     var self = this
     bg = bg ? bg : false
     return function() {
-      return self.output([].slice.call(arguments).join(' '), bg)
+      return self.output([].slice.call(arguments).join(' '), self.colors(bg))
     }
   },
-	nextColor: function(s, bg) {
-		if(s != ' ') { 
-			var color
+	nextColor: function(s, colors) {
+    //not a space
+		if(!this.options.color_space && s == ' ') {
+      return s
+    }
 
-			if(this._prev == this._colors_num) {
-				this._prev = 0, this._next = 1
+		var l = colors.length  - 1
 
-				color = bg ? this.bgify(this._colors[0]) : this._colors[0] 
+		if(this._prev >= l) {
+			this._prev = 0, this._next = 1
 
-				s = ansi[color].open + s + ansi[color].close
-			} else {
+			s = ansi[colors[0]].open + s + ansi[colors[0]].close
+		} else {
 
-				color = bg ? this.bgify(this._colors[this._next]) : this._colors[this._next] 
-				
-        s = ansi[color].open + s + ansi[color].close
-				
-        this._next++
-				this._prev++
-			}
+      s = ansi[colors[this._next]].open + s + ansi[colors[this._next]].close
+			
+      this._next++
+			this._prev++
 		}
 
-		return s
-	},
-	output: function(input, bg) {
-		var l = input.length, output = '', i = 0
+    this._current_gap++
 
-		for (i; i < l; i++) {
-			output += this.nextColor(input.charAt(i), bg)	
+		return s
+	}, 
+  currentColor: function(s, colors) {
+    var l = colors.length  - 1
+
+    if(!this.options.color_space && s == ' ') {
+      return s
+    }
+
+    this._current_gap++
+
+    if(this._prev >= l) {
+      return ansi[colors[0]].open + s + ansi[colors[0]].close
+    } else {
+      return ansi[colors[this._next]].open + s + ansi[colors[this._next]].close
+    }
+
+  },
+	output: function(input, colors) {
+		var l = input.length, output = '', i = 0, gap = this.options.gap, s
+
+    this._current_gap = 1
+		
+    for (i; i < l; i++) {
+      this._current_gap = this._current_gap > gap ? 1 : this._current_gap
+		  output += this._current_gap == gap ? this.nextColor(input.charAt(i), colors) : this.currentColor(input.charAt(i), colors)
 		}
 
 		//reset state
-		this._next = 0, this._prev = 0
+		this._next = 0, this._prev = -1
 
 		return output
 	}
 }
 
-var pony = new Pony()
-
-module.exports = pony.ponyfy()
-module.exports.bg = pony.ponyfy(true)
+module.exports = new Pony()
